5fa204252589f52e1421a50b7c9bb12e8f59383c
Ali Sobhani <acdota@outlook.com>
2023-10-20 23:19:58 +0330

    Add project files.

---
+++ Portfolio Project/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio_Project.Models;
+using System.Diagnostics;
+
+namespace Portfolio_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult GetJsonDataTest()
+        {
+            var myList = new List<string>();
+            myList.Add("Ali");
+            myList.Add("Mohammad");
+            myList.Add("Reaza");
+            myList.Add("Nazanin");
+            myList.Add("Neda");
+            myList.Add("Parastoo");
+
+            return Json(new { myList });
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
