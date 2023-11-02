683999c831b2bd024f9fa1e2d3da870edc518a74
Ali Sobhani <acdota@outlook.com>
2023-11-02 20:05:38 +0330

    Portfolio And Client classes are added now

---
+++ Portfolio Project/Models/Client.cs
@@ -0,0 +1,6 @@
+﻿namespace Portfolio_Project.Models
+{
+    public class Client
+    {
+    }
+}
---
+++ Portfolio Project/Models/Protfolio.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Project.Models
+{
+    public class Protfolio
+    {
+        int MAX_NAME_LENGHT = 100;
+        
+
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+
+
+        public Protfolio(int id, string name, string image, string description)
+        {
+            ValidateName(name);
+
+            Id = id;
+            Name = name;
+            Image = image;
+            Description = description;
+        }
+
+
+        public void ValidateName(string name)
+        {
+            if (name.Length > MAX_NAME_LENGHT)
+            {
+                throw new Exception("Name characters are more than 100");
+            }
+        }
+    }
+}
